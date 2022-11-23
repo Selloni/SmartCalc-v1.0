a2e79ce943aa763e3d3ade20e18bc23802b65f6c
Selloni <vip.petrusev@mail.ru>
2022-11-24 02:13:46 +0300

    свет выключили

--- src/smartcalk_QT/credit.cpp
+++ src/smartcalk_QT/credit.cpp
@@ -12,3 +12,41 @@ Credit::~Credit()
 {
     delete ui;
 }
+
+void Credit::on_radioButton_clicked()
+{
+
+    double play_cr;
+    double Sn, In, P;
+    int sum_cr = ui->sum->text().toInt();
+    double percent_cr = ui->procent->text().toDouble();
+    int term_cr = ui->term->text().toInt();
+    int month_all_cr = term_cr * 12;  //  всего месяцев
+    double body_loan = sum_cr/month_all_cr;  // тело займа
+    while (month_all_cr != 0) {
+        sum_cr = sum_cr - sum_cr/month_all_cr;  //  тело постоянно уменьшаеться
+        In = sum_cr * percent_cr / 12;  //  сумма на погошение проценитов
+        ui->output->setText(ui->output->text()+In);
+        P = P + In + body_loan;
+        month_all_cr--;
+    }
+}
+
+void Credit::on_ann_clicked()
+{
+    double play_cr;
+    double Sn, In, P;
+    int sum_cr = ui->sum->text().toInt();
+    double percent_cr = ui->procent->text().toDouble();
+    int term_cr = ui->term->text().toInt();
+    int month_all_cr = term_cr * 12;  //  всего месяцев
+    double body_loan = sum_cr/month_all_cr;  // тело займа
+    while (month_all_cr != 0) {
+        sum_cr = sum_cr - sum_cr/month_all_cr;  //  тело постоянно уменьшаеться
+        In = sum_cr * percent_cr / 12;  //  сумма на погошение проценитов
+        ui->total_month->setText(ui->total_month->text()+In);
+        P = P + In + body_loan;
+        month_all_cr--;
+    }
+}
+
--- src/smartcalk_QT/credit.h
+++ src/smartcalk_QT/credit.h
@@ -15,6 +15,13 @@ public:
     explicit Credit(QWidget *parent = nullptr);
     ~Credit();
 
+private slots:
+    void on_radioButton_clicked();
+
+
+
+    void on_ann_clicked();
+
 private:
     Ui::Credit *ui;
 };
